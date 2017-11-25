3f326c9e374c439237bcbd462739962f06df0813
Zakery Clarke <30702789+zakerytclarke@users.noreply.github.com>
2017-11-25 10:22:55 -0700

    Update txtio.js

--- txtio.js
+++ txtio.js
@@ -1,4 +1,4 @@
-document.write("<h1 id='txtiotitle'>Txt I/O Terminal</h1><p id='txtio' style='font-family: monospace;font: outline;font-size:25px;'></p>");
+document.write("<h1 id='txtiotitle'>Txt I/O Terminal</h1><p id='txtio' style='font-family: monospace;font: outline;font-size:30px;'></p>");
 function textio(){
 alert("Text I/O\nCreated by Zakery Clarke\nMIT License 11/25/2017\n");
 }
@@ -12,7 +12,7 @@ function println(txt){
 document.getElementById("txtio").innerHTML+=txt+"<br>";
 }
 function textsize(size){
-document.getElementById("txtio").style.fontSize=size+"%";
+document.getElementById("txtio").style.fontSize=size+"px";
 }
 function textcolor(color){
 document.getElementById("txtio").style.color=color;
