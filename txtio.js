99ac6767f93e8fd5ad1ea8adfe7a1811b92175c8
Zakery Clarke <30702789+zakerytclarke@users.noreply.github.com>
2017-11-28 09:26:40 -0700

    Update txtio.js

--- txtio.js
+++ txtio.js
@@ -1,4 +1,4 @@
-document.write("<p id='txtio' style='font-family:monospace;font:outline;font-size:30px;width:100%;height:100%;position:absolute;top:0;left:10;'>Txt I/O Terminal<br></p>");
+document.write("<p id='txtio' style='font-family:monospace;font:outline;font-size:30px;width:100%;height:100%;position:absolute;padding:30px;top:0px;left:20px;'>Txt I/O Terminal<br>Created by Zakery Clarke<br>MIT License 11/25/17<br></p>");
 function textio(){
 alert("Text I/O\nCreated by Zakery Clarke\nMIT License 11/25/2017\n");
 }
@@ -26,4 +26,19 @@ alert(txt);
 function input(msg){
 return prompt(msg);  
 }
- 
+function txtiohelp(){
+cls();
+println("Text I/O");
+println("Created by Zakery Clarke");
+println("MIT License 11/25/2017");
+println("<a href='https://github.com/zakerytclarke/txtio.js/edit/master/txtio.js'>Github</a>"); 
+println("Commands:");
+println("cls() clears the screen");
+println("print(text) prints text to the screen");
+println("println(text) print text to the screen followed by a new line char");
+println("var name=input(msg) inputs a string from a dialog");
+println("toast(msg) shows an alert message");
+println("textcolor(color) sets the text color");
+println("textbgcolor(color) sets the bg color");
+println("textsize(size) sets the text size"); 
+}
